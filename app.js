15f4f93d7724fae860c009ac46877a9ac6ce8704
Sambambamm <165159759+Coder-samm@users.noreply.github.com>
2024-07-29 11:26:56 +0100

    Added a function that gets recipe from another file

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ const PORT = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+app.get('/recipes', (req, res) => {
+try {
+
+}catch{
+  
+}
+}
+
+) 
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
--- recipes.js
+++ recipes.js
@@ -3,8 +3,19 @@ import { v4 as uuidv4 } from "uuid";
 
 const fileName = "recipes.json";
 
+console.log(fileName)
 // GET ALL RECIPES
-export async function getRecipes() {}
+export async function getRecipes() {
+try {
+    const data = await fs.readfile(fileName, 'utf8')
+    const converted = json.parse(data)
+    console.log(converted)
+    return converted
+    }
+    catch (error){
+        console.error('Unable to retrieve recipes')
+    }
+}
 
 // GET A RECIPE BY ID
 export async function getRecipeByID(id) {}
